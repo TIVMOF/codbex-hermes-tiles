766a8702a00e75108ebd2dccdc4a5a9d4f3bb991
TIVMFO <tomyivanov06@gmail.com>
2024-07-03 13:35:34 +0000

    feat: Initial numeric tile

---
+++ codbex-hermes-tiles/tiles.mjs
@@ -0,0 +1,20 @@
+angular.module('applicationTiles', [])
+    .controller('applicationTilesController', ['$scope', function ($scope) {
+        $scope.today = new Date();
+    }])
+    .directive('numericTile', ['SplitPaneState', function (NumericTilePaneState) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: false,
+            scope: {
+                perspective: '@',
+                title: '@',
+                numericData: '@'
+            },
+            controller: ['$scope', '$element', function ($scope, $element) {
+                $scope.today = new Date();
+            }],
+            templateUrl: "/services/web/codbex-athena-tiles/templates/numericTile.html"
+        };
+    }]);
